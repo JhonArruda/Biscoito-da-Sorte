edb9c554b48772c4f4995b736d98dc70eeaf12ca
João Arruda <143569951+JhonArruda@users.noreply.github.com>
2024-04-16 20:17:17 -0300

    Atualização de Css e HTML

---
+++ main.js
@@ -0,0 +1,18 @@
+/*const screen1 = document.querySelector(".screen1")
+const screen2 = document.querySelector(".screen2")
+const btnTry = document.querySelector("#btnTry")
+const btnReset = document.querySelector("#btnReset")
+
+btnTry.addEventListener('click', handleTryClick)
+btnReset.addEventListener('click', handleResetClick)
+document.addEventListener('keydown', function(e) {
+    if(e.key == 'Enter' && screen1.classList.contains('hide')) {
+        handleResetClick()
+    }
+})
+
+function toggleScreen() {
+    screen1.classList.toggle("hide")
+    screen2.classList.toggle("hide")
+}
+*/
