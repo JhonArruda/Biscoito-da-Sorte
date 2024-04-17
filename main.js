3bacccfb611946bfc762e0abe82b3c5753fb7ad2
João Arruda <143569951+JhonArruda@users.noreply.github.com>
2024-04-17 19:53:35 -0300

    Tentativa#1
    Tentativa de fazer as animações corretas e os eventos para a toggleScreen entre divs

--- main.js
+++ main.js
@@ -1,18 +1,10 @@
-/*const screen1 = document.querySelector(".screen1")
-const screen2 = document.querySelector(".screen2")
-const btnTry = document.querySelector("#btnTry")
-const btnReset = document.querySelector("#btnReset")
-
-btnTry.addEventListener('click', handleTryClick)
-btnReset.addEventListener('click', handleResetClick)
-document.addEventListener('keydown', function(e) {
-    if(e.key == 'Enter' && screen1.classList.contains('hide')) {
-        handleResetClick()
-    }
+document.getElementById('luck').addEventListener('click', function() {
+    const randomIndex = Math.floor(Math.random() * fortuneMessages.length);
+    const message = fortuneMessages[randomIndex];
+    document.getElementById('message').innerText = message;
 })
 
 function toggleScreen() {
     screen1.classList.toggle("hide")
     screen2.classList.toggle("hide")
 }
-*/
